745b06691d7a1480e8b752c16aa8fd8ce2e2cfad
Maricarmen <bozasguerra@gmail.com>
2018-05-02 13:02:12 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,175 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <header>
+
+ <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a href=""><img src="imagen/opendesk_logo.jpg" height="80" alt="OpenDesk"></a>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Furniture <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Workspaces</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Open making</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+                </ul>
+                <span class="navbar-text">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#"> <i class="fas fa-shopping-cart"></i> <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Your dashboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Join as a maker</a>
+                        </li>
+                    </ul>
+                </span>
+            </div>
+        </nav>
+
+            
+    </header>
+
+<div id="uno"></div>
+    <div>
+        <h2>Fourniture designed for <br></h2>
+        <h4>We're changing the wait furniture is made, by conecting customers to local makers</h4>
+        <button class= "btn btn-success"</button>
+    </div>
+</div>
+    
+   
+    
+    
+    
+    <div class="card-group">
+        <div class="card">
+            <img class="card-img-top" src="imagen/foto2.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Select designs</h5>
+                <p class="card-text"><small class="text-muted"><?=('Browse and add disigns to your basket pr get in touch for help'); ?></small></p>
+               
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="imagen/foto3.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Request quotes</h5>
+                <p class="card-text"><small class="text-muted"><?=('Get quotes from local makers in our area within two business days'); ?></small></p>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="imagen/foto4.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Johann Brench</h5>
+                <p class="card-text"><small class="text-muted"><?=('2 person canteen and meeting bench'); ?></small></p>
+                
+            </div>
+        </div>
+    </div>
+    <div class="contenedor">
+    <img id="hombre"src="imagen/fotohombre.jpg" width="100%"/>
+        <div class="texto-encima"></div>
+        <div class="centrado"><h2>Furniture designed for<br> inspiring workplaces</h2><br>
+          <h4>We’re changing the way furniture is<br> made, by connecting customers to local<br> makers<h4>
+             <button type="button" class="btn btn-success">Shop Furniture</button>
+        </div>
+   </div>
+    
+    
+    <div class="card-group">
+        <div class="card">
+            <img class="card-img-top" src="foto.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Select design</h5>
+                    <p class="card-text"><small class="text-muted"><?=('Browse and add disign to your basket or get in touch for help'); ?></small></p>
+               
+                </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="foto.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Request quotes</h5>
+                    <p class="card-text"><small class="text-muted"><?=('Get cuotes from local makers in your area within two business days'); ?></small></p>
+                </div>
+        </div>
+        <div class="card">
+                <i class="fas fa-bed"></i>
+            <img class="card-img-top" src="foto.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Choose your maker</h5>
+                    <p class="card-text"><small class="text-muted"><?=('Choose the local maker´s quote that works best for you'); ?></small></p>
+                </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="foto.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Receive your forniture</h5>
+                    <p class="card-text"><small class="text-muted"><?=('Get forniture delivered to your door in 2-4
+                    weeks, direct from your maker'); ?></small></p>
+                </div>
+        </div>
+            <button type="button" class="btn btn-primary btn-lg">Get started</button>
+        </div>
+
+        <div class="container">
+  <div class="row">
+    
+    <img="imagen/logonike.jpg" alt="nike">
+    
+    <img="imagen/logowework.jpg" alt="wework">
+    
+    <img="imagen/logogoogle.jpg" alt="google">
+    
+    <img="imagen/logogreenpeace.jpg" alt="greenpeace"> 
+    
+  </div>
+
+           
+            
+
+
+
+       
+
+
+
+
+      
+
+    <footer>
+        
+    </footer>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="app.js"></script>
+</body>
+</html>
